7e131196bb8ba97c1edd096653ff01c5eb349621
Prabhas <115615425+Prabhas505@users.noreply.github.com>
2024-06-05 16:04:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ OWNER_ID = int(getenv("OWNER_ID", 6440277281))
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Danishzain05/SpartanGpt")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Prabhas505/Prabhas-Daxx-New-Musicrepo-")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
